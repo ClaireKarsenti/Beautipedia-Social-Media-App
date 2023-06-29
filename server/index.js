35b06125994039201f97c93faae6cec37a5f6da7
Claire Karsenti <claire.karsenti.web@gmail.com>
2023-06-29 12:43:31 +0200

    File storage

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import multer from 'multer';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
@@ -20,3 +21,14 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+  destination: function (req, file, cd) {
+    cb(null, 'public/assets');
+  },
+  filename: function (req, file, cd) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
