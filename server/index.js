215986d4ae029fb5161d477952da25f82573735e
Claire Karsenti <claire.karsenti.web@gmail.com>
2023-06-29 20:44:59 +0200

    Add user routes

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth.js';
+import userRoutes from './routes/users.js';
 import { register } from './controllers/auth.js';
 
 
@@ -41,3 +42,4 @@ app.post('/auth/register', upload.single('picture'), register);
 
 /* ROUTES */
 app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
