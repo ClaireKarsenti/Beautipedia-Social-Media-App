085a37db71a91e7f97c2b72653a7f7e45ece792f
Claire Karsenti <claire.karsenti.web@gmail.com>
2023-06-29 20:54:10 +0200

    Format

--- server/index.js
+++ server/index.js
@@ -18,7 +18,6 @@ import User from './models/User.js';
 import Post from './models/Post.js';
 import { users, posts } from './data/index.js';
 
-
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
