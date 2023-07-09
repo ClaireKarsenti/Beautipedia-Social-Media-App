3c366d2a6ef43e32b8703a758cb6a8ac54397328
Claire Karsenti <claire.karsenti.web@gmail.com>
2023-07-09 18:01:02 +0200

    Typo

--- client/src/scenes/navBar/index.jsx
+++ client/src/scenes/navBar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          BeautyPedia
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
