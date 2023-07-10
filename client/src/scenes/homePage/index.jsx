76ea2aa645f147c68d6342a2cf7ea5ef2ff98e9f
Claire Karsenti <claire.karsenti.web@gmail.com>
2023-07-10 20:42:51 +0200

    Add MyPostWidget to homePage

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { Box, useMediaQuery } from '@mui/material';
 import Navbar from 'scenes/navBar';
 import UserWidget from 'scenes/widgets/UserWidget';
+import MyPostWidget from 'scenes/widgets/MyPostWidget';
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery('(min-width:1000px)');
@@ -21,6 +22,17 @@ const HomePage = () => {
         <Box flexBasis={isNonMobileScreens ? '26%' : undefined}>
           <UserWidget userId={_id} picturePath={picturePath} />
         </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? '42%' : undefined}
+          mt={isNonMobileScreens ? undefined : '2rem'}
+        >
+          <MyPostWidget picturePath={picturePath} />
+        </Box>
+        {isNonMobileScreens && (
+          <Box flexBasis="26%">
+            <Box m="2rem 0" />
+          </Box>
+        )}
       </Box>
     </Box>
   );
