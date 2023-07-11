7c6cc0425876ca8c8e83bf09bc8c31c4816ad6a2
Claire Karsenti <claire.karsenti.web@gmail.com>
2023-07-11 21:26:38 +0200

    Add PostsWidget to homePage

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -4,6 +4,7 @@ import { Box, useMediaQuery } from '@mui/material';
 import Navbar from 'scenes/navBar';
 import UserWidget from 'scenes/widgets/UserWidget';
 import MyPostWidget from 'scenes/widgets/MyPostWidget';
+import PostsWidget from 'scenes/widgets/PostsWidget';
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery('(min-width:1000px)');
@@ -27,6 +28,7 @@ const HomePage = () => {
           mt={isNonMobileScreens ? undefined : '2rem'}
         >
           <MyPostWidget picturePath={picturePath} />
+          <PostsWidget userId={_id} />
         </Box>
         {isNonMobileScreens && (
           <Box flexBasis="26%">
