49a0918d2291b34f01c62e2432479a78b6935191
Claire Karsenti <claire.karsenti.web@gmail.com>
2023-07-15 20:41:02 +0200

    Adjust color accessibility

--- client/src/theme.js
+++ client/src/theme.js
@@ -40,7 +40,7 @@ export const themeSettings = (mode) => {
             primary: {
               dark: colorTokens.primary[200],
               main: colorTokens.primary[500],
-              light: colorTokens.primary[800],
+              light: colorTokens.primary[700],
             },
             neutral: {
               dark: colorTokens.grey[100],
@@ -59,7 +59,7 @@ export const themeSettings = (mode) => {
             primary: {
               dark: colorTokens.primary[700],
               main: colorTokens.primary[500],
-              light: colorTokens.primary[50],
+              light: colorTokens.primary[300],
             },
             neutral: {
               dark: colorTokens.grey[700],
