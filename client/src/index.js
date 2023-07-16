068083d71251fde5092e56bac35d2070667b5880
Claire Karsenti <claire.karsenti.web@gmail.com>
2023-07-16 21:36:03 +0200

    Disable redux devtools

--- client/src/index.js
+++ client/src/index.js
@@ -17,6 +17,9 @@ import {
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { PersistGate } from 'redux-persist/integration/react';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 const persistConfig = { key: 'root', storage, version: 1 };
 const persistedReducer = persistReducer(persistConfig, authReducer);
@@ -28,6 +31,7 @@ const store = configureStore({
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
     }),
+  devTools: false,
 });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
